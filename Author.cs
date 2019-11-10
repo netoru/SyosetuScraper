7fec4c6d3daee1cd5521458ec0653497560cfe3c
Netoru <kuroame71@gmail.com>
2019-11-10 17:50:28 +0100

    adding support for series and author links

---
+++ Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyosetuScraper
+{
+    class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Novel> Novels { get; set; }
+        public List<Series> SeriesList { get; set; }
+    }
+}
---
+++ Series.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyosetuScraper
+{
+    class Series
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Novel> Novels { get; set; }
+    }
+}
